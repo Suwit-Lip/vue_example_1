4a43b067b865b17ca8af634b3e572eb87ec59777
suwit.i <>
2024-12-05 15:25:00 +0700

    EventCard Exercise part 2

---
+++ src/components/BookingItem.vue
@@ -0,0 +1,16 @@
+<script setup>
+import SectionCard from '@/components/SectionCard.vue';
+import RoundButton from '@/components/RoundButton.vue';
+defineProps({
+  title: String,
+});
+</script>
+
+<template>
+  <SectionCard>
+    <div class="flex justify-between">
+      <div>{{ title }}</div>
+      <RoundButton variant="danger">Cancel</RoundButton>
+    </div>
+  </SectionCard>
+</template>
---
+++ src/components/EventCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <SectionCard>
+    <template #header>
+      {{ title }}
+    </template>
+    {{ when }}
+    <template #footer>
+      <div class="space-y-4">
+        <p>{{ description }}</p>
+        <section class="flex justify-end p-4">
+          <RoundButton @click="$emit('register')">Register</RoundButton>
+        </section>
+      </div>
+    </template>
+  </SectionCard>
+</template>
+<script setup>
+import RoundButton from '@/components/RoundButton.vue';
+import RoundCard from '@/components/RoundCard.vue';
+import SectionCard from '@/components/SectionCard.vue';
+defineProps({
+  title: String,
+  when: String,
+  description: String,
+});
+defineEmits(['register']);
+</script>
