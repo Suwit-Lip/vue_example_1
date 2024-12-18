50e78a06bc8bdca0f911dc6248d0763bf75e3e31
suwit.i <>
2024-12-18 21:11:20 +0700

    finish project

--- src/composables/useBookings.js
+++ src/composables/useBookings.js
@@ -2,12 +2,16 @@ import { ref } from 'vue';
 
 const bookings = ref([]);
 const loading = ref(false);
+const error = ref(null);
 
 const fetchBookings = async () => {
   loading.value = true;
+  error.value = null;
   try {
     const response = await fetch('http://localhost:3002/bookings');
     bookings.value = await response.json();
+  } catch (e) {
+    error.value = e;
   } finally {
     loading.value = false;
   }
@@ -72,6 +76,7 @@ export default function useBookings() {
   return {
     bookings,
     loading,
+    error,
     fetchBookings,
     handlerRegistration,
     cancelBooking,
